e5e0752c75d64d3a0dfb9045a26e65c90341eff6
Komal Harshita <komal.sony234@gmail.com>
2025-08-27 16:14:04 +0530

    added java files

---
+++ BASICS/returnvalue.java
@@ -0,0 +1,12 @@
+public class returnvalue {
+
+    static int mymethod (int num){
+        return num*num ;
+    }
+
+    public static void main(String [] args){
+        int res = returnvalue.mymethod(9);
+        System.out.println("9");
+        System.out.println("Result = "+res);
+    }
+}
